5505e50852f3ddefaa97d30d976a83b21951dcbc
ihor_fomin <ihor.fomin.mypage@gmail.com>
2021-05-07 13:19:18 +0300

    https://github.com/IhorFomin/WeatherWebApp
    first commit

---
+++ weather/views.py
@@ -0,0 +1,30 @@
+import requests
+from django.shortcuts import render
+from .models import City
+from .forms import CityForm
+
+def index(request):
+    appid = '2a2d8171f2fe87038519f3f9d43747e8'
+    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + appid
+
+    if(request.method == 'POST'):
+        form = CityForm(request.POST)
+        form.save()
+
+    form = CityForm()
+
+    cities = City.objects.all()
+    all_cities = []
+
+    for city in cities:
+        res = requests.get(url.format(city.name)).json()
+        city_info = {
+            'city': city.name,
+            'temp': res["main"]["temp"],
+            'icon': res["weather"][0]["icon"]
+        }
+        all_cities.append(city_info)
+
+    context = {'all_info': all_cities, 'form': form}
+
+    return render(request, 'weather/index.html', context)
